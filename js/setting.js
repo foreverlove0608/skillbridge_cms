11a665629b1e9407e19f121f3e7910af1651a780
Ibis Air <>
2024-02-25 17:50:02 +0700

    up code

---
+++ js/setting.js
@@ -0,0 +1,23 @@
+$('.js-lang').click(function () {
+    $(this).next().slideToggle();
+
+    $('.switch-language__list li a').click(function () {
+        var lang_value = $(this).html();
+        $('.switch-language__label').html(lang_value);
+        $('.switch-language__list').slideUp();
+    })
+});
+
+$('.js-user').click(function () {
+  $(this).next().slideToggle();
+  $(this).toggleClass('is-active');
+})
+
+$(".toggle-pass").click(function() {
+	var input = $($(this).attr("toggle"));
+	if (input.attr("type") == "password") {
+	  input.attr("type", "text");
+	} else {
+	  input.attr("type", "password");
+	}
+  });
