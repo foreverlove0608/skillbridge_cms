d136ce04a8e61d87d23028487e1701bc5af9249e
Ibis Air <>
2024-02-27 01:05:45 +0700

    up

--- js/setting.js
+++ js/setting.js
@@ -1,23 +1,29 @@
 $('.js-lang').click(function () {
-    $(this).next().slideToggle();
+		$(this).next().slideToggle();
 
-    $('.switch-language__list li a').click(function () {
-        var lang_value = $(this).html();
-        $('.switch-language__label').html(lang_value);
-        $('.switch-language__list').slideUp();
-    })
+		$('.switch-language__list li a').click(function () {
+				var lang_value = $(this).html();
+				$('.switch-language__label').html(lang_value);
+				$('.switch-language__list').slideUp();
+		})
 });
 
 $('.js-user').click(function () {
-  $(this).next().slideToggle();
-  $(this).toggleClass('is-active');
+	$(this).next().slideToggle();
+	$(this).toggleClass('is-active');
 })
 
 $(".toggle-pass").click(function() {
 	var input = $($(this).attr("toggle"));
 	if (input.attr("type") == "password") {
-	  input.attr("type", "text");
+		input.attr("type", "text");
 	} else {
-	  input.attr("type", "password");
+		input.attr("type", "password");
 	}
-  });
+	});
+
+
+$('.btn-action').click(function () {
+	$(this).next().slideToggle();
+	$(this).toggleClass('is-active');
+})
